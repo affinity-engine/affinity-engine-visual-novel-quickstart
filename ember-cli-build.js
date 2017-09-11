95fc8a769a87e5ecd4fb7858439e4e39178bfce2
null null null <null@affinitygamescollective.org>
2017-09-11 13:30:45 -0400

    0.2.0

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,14 +1,11 @@
-/*jshint node:true*/
-/* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+/* eslint-env node */
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
+  let app = new EmberApp(defaults, {
     // Add options here
-    'ember-cli-babel': {
-      includePolyfill: true,
-      stage: 1
-    }
   });
 
   // Use `app.import` to add additional libraries to the generated
